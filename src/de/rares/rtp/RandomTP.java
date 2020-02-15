4873c1937f33c6248226d5642542eac3839f020b
Rares <rsahleanu@gmail.com>
2020-02-15 16:59:35 +0100

    Initial commit

---
+++ src/de/rares/rtp/RandomTP.java
@@ -0,0 +1,16 @@
+package de.rares.rtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandomTP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/de/rares/rtp/listeners/TPListener.java
@@ -0,0 +1,15 @@
+package de.rares.rtp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class TPListener implements Listener {
+
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e){
+        
+    }
+
+}
